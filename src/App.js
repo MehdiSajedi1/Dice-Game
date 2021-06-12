b6c468bb263b09d2c19c6bdc537e0582cb434b23
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-06-12 15:22:56 -0700

    added autoplay / interval still runs on game completion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Player from './components/Player';
 import Dice from './components/Dice';
 import Button from './components/Button';
 import './App.css';
 
-const gamePoint = 100;
+const gamePoint = 30;
 
 function App() {
   const [activePlayer, setActivePlayer] = useState(true);
@@ -20,7 +20,8 @@ function App() {
     }
   }, [currentRoll, setActivePlayer]);
 
-  const rollDice = () => {
+  const rollDice = useCallback(() => {
+    console.log('tryna run rollDice');
     if (score1 >= gamePoint || score2 >= gamePoint) return;
 
     const roll = Math.floor(Math.random() * 6 + 1);
@@ -29,9 +30,10 @@ function App() {
     if (roll !== 1) {
       setCurrentScore((prevRoll) => prevRoll + roll);
     }
-  };
+  }, [score1, score2]);
 
-  const hold = () => {
+  const hold = useCallback(() => {
+    console.log('tryna run hold');
     if (score1 >= gamePoint || score2 >= gamePoint) return;
 
     switch (activePlayer) {
@@ -46,11 +48,9 @@ function App() {
       default:
         break;
     }
-
     setCurrentScore(0);
-
     setActivePlayer((prevActive) => !prevActive);
-  };
+  }, [activePlayer, currentScore, score1, score2]);
 
   const newGame = () => {
     setScore1(0);
@@ -59,6 +59,27 @@ function App() {
     setCurrentScore(0);
     setCurrentRoll(0);
   };
+
+  // AUTOPLAY - Comment out for user play
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     rollDice();
+  //   }, 500);
+
+  //   if (activePlayer) {
+  //     if (score1 + currentScore >= gamePoint) hold();
+  //   }
+
+  //   if (!activePlayer) {
+  //     if (score2 + currentScore >= gamePoint) hold();
+  //   }
+
+  //   if (currentScore >= 20) {
+  //     hold();
+  //   }
+
+  //   return () => clearInterval(interval);
+  // }, [score1, score2, currentScore, activePlayer, hold, rollDice]);
 
   return (
     <main className="App">
